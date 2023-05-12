06db452c4f63cb6be91e5d8a3aa7512103203f58
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-05-12 21:17:18 +1000

    v3

---
+++ hw-1.js
@@ -0,0 +1,40 @@
+// Задание 1
+// let a = 20;
+// alert(a);
+
+// // Задание 2
+// const iPhoneDate = 2005;
+// alert(iPhoneDate);
+
+// // Задание 3
+// const CoderJS = 'Bob';
+// alert(CoderJS);
+
+// Задание 4
+// let num_4_1 = 10;
+// let num_4_2 = 2;
+// let result_4_1 = num_4_1 + num_4_2;
+// let result_4_2 = num_4_1 - num_4_2;
+// let result_4_3 = num_4_1 * num_4_2;
+// let result_4_4 = num_4_1 / num_4_2;
+
+// alert(result_4_1);
+// alert(result_4_2);
+// alert(result_4_3);
+// alert(result_4_4);
+
+// Задание 5
+    let num_5_1 = 2;
+    let result_5_1 = num_5_1**5;
+    alert(result_5_1);
+
+// Задание 6
+
+// Задание 7
+
+// Задание 8
+
+// Задание 9
+
+// Задание 10
+
