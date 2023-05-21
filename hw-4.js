8e1e05505da4bf1d5d7f136683f5034819e7ab6f
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-05-21 19:32:16 +1000

    hw-4 v.1

---
+++ hw-4.js
@@ -0,0 +1,45 @@
+// Задание 1
+// for (let i = 1; i <= 2; i++) {
+//     alert('Привет');
+// }
+
+// Задание 2
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// Задание 3
+// for (let i = 7; i <= 22; i++) {
+//     console.log(i);
+// }
+
+// Задание 4
+// let salary = {
+//     'Коля': '200',
+//     'Вася': '300',
+//     'Петя': '400',
+// }
+
+// for (let man in salary) {
+//     if (salary[man]) {
+//         console.log(`${man}, получает зарплату ${salary[man]} долларов`);
+//     }
+// }
+
+// Задание 5
+// for (let m = 1000, num = 0; m > 50; m /= 2, num++) {
+//     console.log(m);
+//     console.log(num);
+// }
+
+// Задание 6
+// let friday = Number(prompt('введите дату первой пятницы в этом месяце'));
+
+// for (dayNumber = friday; dayNumber <= 31; dayNumber += 7) {
+//     if (friday <= 7 && friday != 0) {
+//         console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`); 
+//     }
+// }
+
+
+
